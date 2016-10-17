918f70e52d42a7ebf6b0540c87f572ba4ea2c279
regenboogsjaal <ivybarend@gmail.com>
2016-10-17 18:14:53 +0200

    submarine scene kees en wouter

---
+++ Submarine_Mayhem/Assets/Random_Movement.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//float MAX_STARTING_SPEED = 50;
+
+public class Random_Movement : MonoBehaviour {
+
+ void Start() {
+         GetComponent<Rigidbody>().velocity = Random.insideUnitCircle * (0.5f + 0.5f * Random.value) * 50;
+     }
+}
---
+++ Submarine_Mayhem/Assets/script/PlayAudio.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class PlayAudio : MonoBehaviour {
+	
+    void OnTriggerEnter(Collider Player){
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Play();
+
+    }
+	
+	void OnTriggerExit(Collider Player){
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.Stop();
+
+    }
+}
