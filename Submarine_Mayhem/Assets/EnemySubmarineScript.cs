696a83df4ed13b11e30be34ba6c56b3d86343501
Wouter Bink <wouterbink@live.nl>
2016-10-22 15:53:51 +0200

    Exploding Mines + sound falloff

---
+++ Submarine_Mayhem/Assets/EnemySubmarineScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySubmarineScript : MonoBehaviour
+{
+    public int TorpedoTimer = 4;
+    public float TorpedoSpeed = 1;
+    public GameObject Torpedo;
+    public GameObject Player;
+
+	public void Start()
+    {
+        InvokeRepeating("LaunchTorpedo", 0, TorpedoTimer);
+    }
+
+    public void LaunchTorpedo()
+    {
+        var newTorpedo = Instantiate(Torpedo);
+
+        newTorpedo.GetComponent<Rigidbody>().velocity = (transform.position - Player.transform.position).normalized * TorpedoSpeed;
+    }
+}
---
+++ Submarine_Mayhem/Assets/script/MineScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MineScript : MonoBehaviour
+{
+
+    public GameObject Player;
+    public AudioSource Explosion;
+    public int MineRadius = 2;
+
+    private bool exploding = false;
+
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (Vector3.Distance(transform.position, Player.transform.position) < MineRadius && !exploding)
+	    {
+	        exploding = true;
+	        foreach (var audioSource in FindObjectsOfType(typeof(AudioSource)) as AudioSource[])
+	        {
+                audioSource.volume = 0;
+	        }
+	        Explosion.volume = 1;
+	        Explosion.PlayOneShot(Explosion.clip);
+	        StartCoroutine(WaitForSound(Explosion.clip));
+	    }
+	}
+
+    IEnumerator WaitForSound(AudioClip clip)
+    {
+        yield return new WaitForSeconds(clip.length);
+        SceneManager.LoadScene("blabla");
+    }
+}
